c2d6ace440e82238d09dc5523dc316d3ef734fb4
mickey hailu <mickeyhailu20@gmail.com>
2024-09-09 12:18:53 +0300

    sidebar menu update

--- src/components/SideBar/Menu.tsx
+++ src/components/SideBar/Menu.tsx
@@ -15,12 +15,13 @@ const Menu: React.FC<MenuProps> = ({ menuItems }) => {
   const menuItemStyles = css`
     display: flex;
     align-items: center;
-    padding: 10px;
+    padding-left: 5px;
+    padding-right: 5px;
     // width: 100%;
     // border: 1px solid #34495e; /* Border around each nav link */
     border-radius: 4px;
     transition: background-color 0.3s ease, color 0.3s ease;
-    margin-bottom: 10px;
+    margin-bottom: 2px;
 
     &:hover {
       background-color: #1abc9c; /* Hover background color */
@@ -38,16 +39,24 @@ const Menu: React.FC<MenuProps> = ({ menuItems }) => {
       font-size: 16px;
     }
   `;
+  const linkStyle = css`
+  color: inherit;
+  text-decoration: none;
+  font-size: 16px;
+  `
   return (
     <>
       <ul style={{width: '100%', padding: '0'}}>
         {menuItems.map((menuItem) => (
+          <Link to={menuItem.path} className={`${linkStyle}`}>
           <li key={menuItem.id} className={`${menuItemStyles}`}>
-            <span>
+            
               {menuItem.icon}{" "}
-              <Link to={menuItem.path}>{menuItem.menuName}</Link>
-            </span>
+              <h5>{menuItem.menuName}</h5>
+              {/* <Link to={menuItem.path}>{menuItem.menuName}</Link> */}
+          
           </li>
+          </Link>
         ))}
       </ul>
     </>
